f34a2adadfbca18b90f386f7aa6da52dd67bef7e
nrscplus <79967241+nrscplus@users.noreply.github.com>
2021-03-04 18:39:33 +0800

    Create Base.sol

---
+++ Base.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.12;
+
+import './libraries/SafeMath.sol';
+
+contract Base {
+    using SafeMath for uint;
+    // TODO
+    address public constant USDT_ADDR = address(0x41FDB4F3F02E2CCEC1D758FE850F52ECFAE128EC69);
+}
